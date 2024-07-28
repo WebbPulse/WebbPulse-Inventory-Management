9312f11fc16d822ea62fc299389ae1970345ca27
Tyler <tylert2610@gmail.com>
2024-07-28 16:40:19 -0700

    move device logic server side

--- functions/src/shared/shared.py
+++ functions/src/shared/shared.py
@@ -3,6 +3,6 @@ from firebase_admin import firestore, auth
 import google.cloud.firestore as gcf
 from typing import Any
 
-allowed_domains = ["verkada.com", "gmail.com"]
+allowed_domains = ["verkada.com"]
 POSTcorsrules=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"])
 db = firestore.client()
