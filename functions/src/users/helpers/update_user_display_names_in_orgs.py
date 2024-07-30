21476fe1de9f8c48531f325e4ab7b16639346f2d
Tyler <tylert2610@gmail.com>
2024-07-29 23:44:25 -0700

    Update update_user_display_names_in_orgs.py

--- functions/src/users/helpers/update_user_display_names_in_orgs.py
+++ functions/src/users/helpers/update_user_display_names_in_orgs.py
@@ -2,7 +2,7 @@ from src.shared.shared import db
 
 def update_user_display_names_in_orgs(org_id, uid, display_name):
     try:
-        db.collection('organizations').document(org_id).collection('members').document(uid).set({
+        db.collection('organizations').document(org_id).collection('members').document(uid).update({
             'displayName': display_name,
         })
     except Exception as e:
