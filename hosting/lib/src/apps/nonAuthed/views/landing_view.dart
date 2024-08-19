1a1943bc4ade20ea93365951792c46b06908f917
Tyler2610 <tyler.webb@verkada.com>
2024-08-19 11:06:28 -0700

    fixing testing

--- hosting/lib/src/apps/nonAuthed/views/landing_view.dart
+++ hosting/lib/src/apps/nonAuthed/views/landing_view.dart
@@ -17,7 +17,7 @@ class LandingView extends StatelessWidget {
           // Background image
           Positioned.fill(
             child: Image.asset(
-              'assets/saber.gif', // Your image asset path
+              'boxes.jpg', // Your image asset path
               fit: BoxFit.cover,
             ),
           ),
