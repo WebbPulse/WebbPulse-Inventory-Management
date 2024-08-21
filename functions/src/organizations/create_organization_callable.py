60479816cb56fd32a79b6f42f1e9ff5975c17de5
Tyler2610 <tyler.webb@verkada.com>
2024-08-21 12:54:01 -0700

    Update create_organization_callable.py

--- functions/src/organizations/create_organization_callable.py
+++ functions/src/organizations/create_organization_callable.py
@@ -6,7 +6,6 @@ from src.users.helpers.update_user_roles import update_user_roles
 @https_fn.on_call(cors=POSTcorsrules)
 def create_organization_callable(req: https_fn.CallableRequest) -> Any:
     try:
-        
         # Extract parameters
         org_name = req.data["orgName"]
         uid = req.auth.uid
@@ -17,8 +16,6 @@ def create_organization_callable(req: https_fn.CallableRequest) -> Any:
         check_user_is_authed(req)
         check_user_token_current(req)
 
-        
-
         # Check if the organization_creation_name is provided and valid
         if not org_name:
             raise https_fn.HttpsError(
@@ -28,14 +25,12 @@ def create_organization_callable(req: https_fn.CallableRequest) -> Any:
 
         # Create the organization in Firestore
         org_ref = db.collection('organizations').document()
+        org_id = org_ref.id  # Directly use the generated ID
         org_ref.set({
-            'orgId': org_ref.id,
+            'orgId': org_id,
             'createdAt': firestore.SERVER_TIMESTAMP,
             'orgName': org_name,
         })
-
-        # Retrieve the newly created organization UID
-        org_id = db.collection('organizations').where('orgName', '==', org_name).get()[0].id
 
         # Update user organizations and add user to the organization
         update_user_organizations(uid, org_id)
