c99e66570661a50051a913e43a8f6f9d301ecbb3
Tyler <tylert2610@gmail.com>
2024-09-18 22:00:39 -0700

    Remove Allowed Email Domain Logic

--- functions/src/shared.py
+++ functions/src/shared.py
@@ -5,7 +5,6 @@ import google.cloud.firestore as gcf
 from typing import Any
 import time
 
-allowed_domains = ["gmail.com","verkada.com"]
 POSTcorsrules=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"])
 db = firestore.client()
 
