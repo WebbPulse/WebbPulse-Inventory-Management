259d96eda41d5f8575180c0e8e5d014386bf2109
Tyler2610 <tyler.webb@verkada.com>
2024-10-16 14:36:21 -0700

    remove google sign in

--- hosting/lib/src/shared/authentication_provider_list.dart
+++ hosting/lib/src/shared/authentication_provider_list.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart'; // Google OAuth provider
-import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart'; // Apple OAuth provider
+// Apple OAuth provider
 
 List<AuthProvider> authenticationProviderList = [
   EmailAuthProvider(), // Enable email/password sign-in
-  GoogleProvider(
-      clientId:
-          '1096617861007-ek22rdfust1c288m6cl2fq8649i7albp.apps.googleusercontent.com'), // Enable Google sign-in and supply required client ID
+  ///GoogleProvider(
+      ///clientId:
+          ///'1096617861007-ek22rdfust1c288m6cl2fq8649i7albp.apps.googleusercontent.com'), // Enable Google sign-in and supply required client ID
 ];
 
 
 /// apple provider removed for now
+/// same with google sign on
+/// both will need to be added at the same time to make apple not be mad
