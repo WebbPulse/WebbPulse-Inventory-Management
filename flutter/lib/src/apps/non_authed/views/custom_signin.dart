927c342392eba9c27d5c79939263719528bcc98d
Tyler2610 <tyler.webb@verkada.com>
2024-10-29 13:47:22 -0700

    detect inactivative invite links and respond to it in the UI

--- flutter/lib/src/apps/non_authed/views/custom_signin.dart
+++ flutter/lib/src/apps/non_authed/views/custom_signin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:webbpulse_inventory_management/src/shared/providers/authentication_change_notifier.dart';
+import 'landing_view.dart';
 
 class CustomSignInView extends StatelessWidget {
   final String? token;
@@ -11,19 +12,36 @@ class CustomSignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (token != null) {
-      final authProvider =
-          Provider.of<AuthenticationChangeNotifier>(context, listen: false);
-      authProvider.signInWithCustomToken(token!);
+    final authProvider = Provider.of<AuthenticationChangeNotifier>(context);
+
+    // Trigger sign-in only if the token exists and there's no error
+    if (token != null && authProvider.authError == null) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        authProvider.signInWithCustomToken(token!);
+      });
     }
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Custom Sign-In")),
+      appBar: AppBar(
+        title:const Text("Custom Sign-In"),
+        actions: [
+          ElevatedButton.icon(
+            label: const Text("Sign In"),
+            icon: const Icon(Icons.login),
+            onPressed: () {
+              Navigator.pushNamed(context, LandingView.routeName);
+            },
+          ),
+        ],
+        ),
       body: Center(
-        child: token != null
-            ? const CircularProgressIndicator()
-            : const Text("No token found in the URL"),
+        child: authProvider.authError != null
+            ? Text(authProvider.authError!) // Display error message if token is invalid
+            : token != null
+                ? const CircularProgressIndicator() // Show loading if token is valid and signing in
+                : const Text("No token found in the URL"), // No token present in the URL
       ),
     );
   }
 }
+
